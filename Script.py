c0cebac4a6baa0597a152f090b4283de8db03eff
Neil Botelho <neil.botelho321@gmail.com>
2019-03-10 21:23:41 +0530

    Fixed comments in script.py

--- Script.py
+++ Script.py
@@ -7,11 +7,14 @@ import pytz
 
 f=h5py.File("1541962108935000000_167_838.h5",'r')
 
+#The data to be used in the csv file will be added to the csvTemplate variable
 csvTemplate=[['ObjectType','Address','Name','Size','Shape','DataType']]
 
 def DatasetCheck(name, node):
-    """ This function takes the '/' separated address of an item in the hdf file and the item itself as parameters. 
-    If the item is a Dataset it appends the current items address, name, size, shape, and the type of data in contains to the variable csvTemplate"""
+    """ This function takes the '/' separated address of an item in the 
+    hdf file and the item itself as parameters. If the item is a Dataset it appends
+    the current item's address, name, size, shape and the type of data it contains 
+    to the variable csvTemplate"""
     
     global csvTemplate    
     
@@ -43,8 +46,10 @@ def DatasetCheck(name, node):
             csvTemplate.append(out)
         
 
-#Making the CSV file
-#visititems() recursively gets every item(i.e group, dataset etc) and its address in the hdf file and passes it to a specified callable function
+#Making the CSV file:
+
+#visititems() recursively gets every item(i.e group, dataset etc) and its address in the hdf file
+#and passes it to a specified callable function
 f.visititems(DatasetCheck)    
 with open('ScriptOutput.csv','w') as h:
     for row in csvTemplate:
@@ -54,7 +59,7 @@ with open('ScriptOutput.csv','w') as h:
         h.write(",".join(rowstring)+"\n")
 
 
-#Plotting the Image
+#Plotting the Image:
 imgHt=np.array(f['AwakeEventData']['XMPP-STREAK']['StreakImage'].get('streakImageHeight'))[0]
 imgWdth=np.array(f['AwakeEventData']['XMPP-STREAK']['StreakImage'].get('streakImageWidth'))[0]
 img=np.array(f['AwakeEventData']['XMPP-STREAK']['StreakImage'].get('streakImageData'))
@@ -62,7 +67,8 @@ img=medfilt(np.reshape(img,(imgHt,imgWdth)))
 plt.imshow(img)
 plt.savefig('XMPP-STREAK-Image.png')
 
-#Creating the Datetime objects
+
+#Creating the Datetime objects:
 timestamp=round(1541962108935000000/(10**(9)))
 date_str = datetime.datetime.fromtimestamp(timestamp)
 datetime_obj_UTC = datetime.datetime.strptime(str(date_str), "%Y-%m-%d %H:%M:%S")
